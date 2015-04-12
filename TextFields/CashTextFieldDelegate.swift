a20fe14a4dc0c0dc5197c71adeef2cb1a7574046
Chrisna Aing <chrisna@chrisna.org>
2015-04-12 01:04:35 -0700

    Fix maximum length check.

--- TextFields/CashTextFieldDelegate.swift
+++ TextFields/CashTextFieldDelegate.swift
@@ -17,22 +17,17 @@ class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
         }
 
         let newText = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string)
-        let digits = NSCharacterSet.decimalDigitCharacterSet()
-        var newTextDigits = ""
 
         // Digits only in input (or deletions)
+        let digits = NSCharacterSet.decimalDigitCharacterSet()
         for character in string.unicodeScalars {
             if !digits.longCharacterIsMember(character.value) {
                 return false
             }
         }
 
-        // Max length of 15 ($999,999,999.99)
-        if count(newText) > 15 {
-            return false
-        }
-
         // Gather all digits
+        var newTextDigits = ""
         for character in newText.unicodeScalars {
             if digits.longCharacterIsMember(character.value) {
                 newTextDigits.append(character)
@@ -47,7 +42,14 @@ class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
             let dollars = cents.decimalNumberByDividingBy(100)
             let formatter = NSNumberFormatter()
             formatter.numberStyle = .CurrencyStyle
-            textField.text = formatter.stringFromNumber(dollars)
+            let proposedText = formatter.stringFromNumber(dollars)
+            // Max length of 15 ($999,999,999.99)
+            if let proposedText = proposedText {
+                if count(proposedText) > 15 {
+                    return false
+                }
+            }
+            textField.text = proposedText
         }
 
         return false
